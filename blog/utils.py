a64bf6fd0ef78e6b0725fa2d82c5ad939c63140a
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-11-16 16:35:04 +0000

    Fix: Add explicit CSS sanitizer to bleach sanitization.

--- blog/utils.py
+++ blog/utils.py
@@ -1,22 +1,31 @@
-# blog/utils.py
-
 import bleach
 from markdown import markdown
 from django.conf import settings
+from bleach.css_sanitizer import CSSSanitizer
 
 def sanitize_markdown(markdown_content):
-    # Convert the frozenset to a list
+    # Define allowed tags for sanitization
     allowed_tags = list(bleach.sanitizer.ALLOWED_TAGS) + [
         'p', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
         'br', 'pre', 'code', 'img', 'blockquote',
     ]
 
+    # Define allowed attributes
     allowed_attributes = dict(bleach.sanitizer.ALLOWED_ATTRIBUTES)
     allowed_attributes.update({
         'a': ['href', 'title', 'target', 'rel'],
         'img': ['src', 'alt', 'title'],
-        '*': ['class', 'style'],
+        '*': ['class', 'style'],  # Allow "style" for all elements
     })
+
+    # Define allowed CSS properties if style attributes are used
+    css_sanitizer = CSSSanitizer(
+        allowed_css_properties=[
+            'color', 'font-size', 'font-weight', 'font-style',  # Add more if needed
+            'text-decoration', 'background-color', 'border',
+            'margin', 'padding', 'display', 'width', 'height',
+        ]
+    )
 
     # Convert Markdown to HTML
     html_content = markdown(
@@ -28,6 +37,7 @@ def sanitize_markdown(markdown_content):
         html_content,
         tags=allowed_tags,
         attributes=allowed_attributes,
+        css_sanitizer=css_sanitizer,  # Use CSS sanitizer explicitly
         strip=True,
     )
     return cleaned_content
