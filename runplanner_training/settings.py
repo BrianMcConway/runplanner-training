9178ae0e68fb1407c92642adf507d95f4a9bb956
Brian Mc Conway <brianmcconway@yahoo.co.uk>
2024-11-18 04:57:22 +0000

    DEBUG = True for deployment issue.

--- runplanner_training/settings.py
+++ runplanner_training/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security settings
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 
 # Allowed Hosts
 ALLOWED_HOSTS = [
